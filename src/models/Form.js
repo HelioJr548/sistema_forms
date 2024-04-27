61541c74b82fa06593e07608456c46591279370f
HelioJr548 <heliojr548@gmail.com>
2024-04-27 10:54:04 -0300

    Models

--- src/models/Form.js
+++ src/models/Form.js
@@ -1,22 +1,23 @@
 const { Model, DataTypes } = require('sequelize');
 
 class Form extends Model {
-	static init(sequelize) {
-		super.init(
-			{
-				title: DataTypes.TEXT,
-				description: DataTypes.TEXT,
-			},
-			{
-				sequelize,
-			}
-		);
-	}
+    static init(sequelize) {
+        super.init(
+            {
+                title: DataTypes.TEXT,
+                description: DataTypes.TEXT,
+                active: DataTypes.BOOLEAN,
+            },
+            {
+                sequelize,
+            }
+        );
+    }
 
-	static associate(models) {
-		this.belongsToMany(models.User, { through: 'user_forms' });
-		this.belongsToMany(models.Question, { through: 'form_questions' });
-	}
+    static associate(models) {
+        this.belongsToMany(models.User, { through: 'user_forms' });
+        this.belongsToMany(models.Question, { through: 'form_questions' });
+    }
 }
 
 module.exports = Form;
--- src/models/Question.js
+++ src/models/Question.js
@@ -1,23 +1,23 @@
 const { Model, DataTypes } = require('sequelize');
 
 class Question extends Model {
-	static init(sequelize) {
-		super.init(
-			{
-				body: DataTypes.TEXT,
-				type: DataTypes.TEXT,
-			},
-			{
-				sequelize,
-			}
-		);
-	}
+    static init(sequelize) {
+        super.init(
+            {
+                body: DataTypes.TEXT,
+                type: DataTypes.TEXT,
+            },
+            {
+                sequelize,
+            }
+        );
+    }
 
-	static associate(models) {
-		this.belongsToMany(models.Form, { through: 'form_questions' });
-		this.hasMany(models.Option);
-		this.hasMany(models.Answer);
-	}
+    static associate(models) {
+        this.belongsToMany(models.Form, { through: 'form_questions' });
+        this.hasMany(models.Option);
+        this.hasMany(models.Answer);
+    }
 }
 
 module.exports = Question;
